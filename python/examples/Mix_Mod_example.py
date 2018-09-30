0e66dd24a84b304540eac62121d8562a35345cdf
allera <llera.a1@gmail.com>
2018-09-30 13:15:08 +0200

    Add files via upload

--- python/examples/Mix_Mod_example.py
+++ python/examples/Mix_Mod_example.py
@@ -1,52 +1,42 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Feb 24 14:03:13 2017
-
 @author: allera
 """
 
-#---------
-
-import os 
-import sys
-
-#Modify next line to add the toolbox to path
-toolbox_path = "../code" 
-
-sys.path.append(os.path.join(os.path.abspath(toolbox_path))) 
-
-#--------
-
 
 #Example script to launch fit 1 dimensional mixture model. 
-Inference = 'Method of moments'# OR 'Maximum Likelihood' OR 'Variational Bayes' ML NOT INCLUDED YET
-Inference ='Variational Bayes'
-#Inference ='Maximum Likelihood'
 
+
+#Add the toolbox to path
+import os 
+import sys
+toolbox_path = "../code" 
+sys.path.append(os.path.join(os.path.abspath(toolbox_path))) 
+
+
+#Generate some data
+generate_new_data=1
+if generate_new_data==1:
+    from generate_data_vector2 import generate_data_vector
+    data_vector=generate_data_vector(3, 50000, [0,5,-5], [1,1,1], [0.8, 0.1, 0.1])
+else:
+    import scipy.io as sio
+    sio.loadmat('data_vector.mat')
+
+
+#Define options
+Inference ='Variational Bayes'  #'Method of moments' OR 'Maximum Likelihood' OR 'Variational Bayes' ML NOT INCLUDED YET
 Number_of_Components=3
 Components_Model=['Gauss','InvGamma','-InvGamma'] #Each component can be Gauss, Gamma, InvGamma, -Gamma, -InvGamma
 init_params=[0,1,5,2,-5,2]
 maxits=300
 tol=0.00000001
-
-
-
-from Mixture_Model_1Dim import Mixture_Model_1Dim 
-#from generate_data_vector import generate_data_vector
-#data_vector=generate_data_vector(1)
-
 opts={'Inference':Inference,'Number_of_Components':Number_of_Components,'Components_Model':Components_Model,
                                         'init_params':init_params,'maxits':maxits,'tol':tol}
 
-generate_new_data=1
-if generate_new_data==1:
-    from generate_data_vector2 import generate_data_vector
-    data_vector=generate_data_vector(3, 50000, [0,5,-5], [1,1,1], [0.8, 0.1, 0.1])
-#    import scipy.io as sio
-#    sio.savemat('data_vector.mat', {'data_vector':data_vector})
-else:
-    import scipy.io as sio
-    sio.loadmat('data_vector.mat')
+
+from Mixture_Model_1Dim import Mixture_Model_1Dim     
 Model = Mixture_Model_1Dim(data_vector, opts)
 
 
