fe90557e25d3cc1a904dabd0da896bd2299de23f
allera <llera.a1@gmail.com>
2018-09-30 20:44:17 +0200

    Improve a bit the example

--- python/examples/Mix_Mod_example.py
+++ python/examples/Mix_Mod_example.py
@@ -13,6 +13,7 @@ import os
 import sys
 toolbox_path = "../code" 
 sys.path.append(os.path.join(os.path.abspath(toolbox_path))) 
+#Add the toolbox to path
 
 
 #Generate some data
@@ -23,9 +24,10 @@ if generate_new_data==1:
 else:
     import scipy.io as sio
     sio.loadmat('data_vector.mat')
+#Generate some data
+    
 
-
-#Define options
+#Define options for the mixture model fit
 Inference ='Variational Bayes'  #'Method of moments' OR 'Maximum Likelihood' OR 'Variational Bayes' ML NOT INCLUDED YET
 Number_of_Components=3
 Components_Model=['Gauss','InvGamma','-InvGamma'] #Each component can be Gauss, Gamma, InvGamma, -Gamma, -InvGamma
@@ -34,16 +36,53 @@ maxits=300
 tol=0.00000001
 opts={'Inference':Inference,'Number_of_Components':Number_of_Components,'Components_Model':Components_Model,
                                         'init_params':init_params,'maxits':maxits,'tol':tol}
+#Define options for the mixture model fit
 
 
+# CALL TO FIT MIXTURE MODEL
 from Mixture_Model_1Dim import Mixture_Model_1Dim     
 Model = Mixture_Model_1Dim(data_vector, opts)
+print Model['Mixing Prop.']
+# CALL TO FIT MIXTURE MODEL
+
+
+
+# Plot the resulting fit on a histogram of the data
+import numpy as np
+from alb_MM_functions import invgam
+from alb_MM_functions import gam
+from scipy.stats import norm
+
+my_range=np.linspace(-10,10,10000)
+
+plt0=np.multiply( Model['Mixing Prop.'][0],norm.pdf(my_range,Model['mu1'][0],np.sqrt(np.divide(1,Model['tau1s'][0]))  ) )
+
+if Components_Model[1]=='InvGamma':
+    plt1=np.multiply( Model['Mixing Prop.'][1],invgam(my_range,Model['shapes'][1],Model['scales'][1]))
+elif Components_Model[1]=='Gamma':
+    plt1=np.multiply( Model['Mixing Prop.'][1],gam(my_range,Model['shapes'][1],np.divide(1,Model['rates'][1])))
+    
+plt1[my_range<0]=0
+
+if Components_Model[2]=='-InvGamma':
+    plt2=np.multiply( Model['Mixing Prop.'][2],invgam(-my_range,Model['shapes'][2],Model['scales'][2]))
+elif Components_Model[2]=='-Gamma':
+    plt2=np.multiply( Model['Mixing Prop.'][2],gam(-my_range,Model['shapes'][2],np.divide(1,Model['rates'][2])))
+    
+plt2[my_range>0]=0
 
 
 import matplotlib.pyplot as plt
-#plt.plot(Model['Likelihood']);plt.show()
-#plt.plot(Model['FEs']);plt.show()
-print Model['Mixing Prop.']
+plt.hist(data_vector,bins=50,density=True,alpha=1, color='g')
+plt.plot(my_range,plt0, 'k', linewidth=2)
+plt.plot(my_range,plt1, 'k', linewidth=2)
+plt.plot(my_range,plt2, 'k', linewidth=2)
+plt.plot(my_range,plt0+plt1+plt2, 'r', linewidth=2)
+plt.show()
+
+# Plot the resulting fit on a histogram of the data
+
+
 
 #def plot_fit(data_vector,opts,Model):
 #    #import matplotlib.pyplot as plt
