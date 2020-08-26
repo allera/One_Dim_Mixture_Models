Fixed overflow issue with Inverse Gammas bu adding control on overflow of gamma function of the alpha parameter of the inv gamma to the alpha and beta estimations through method of moments. I keep (for no reason) the old functions named now as alpha_IG_raw and beta_IG_raw. I could apply something like this to the Gamma models also. However the issue almost never appeared. I fixed it for a few subjects from UK biobank

python/code/alb_MM_functions.py
@@ -179,11 +179,34 @@ def varIG(alpha,beta):
 
 def alphaIG(mu,var):
     aa=np.true_divide(np.power(mu, 2),var)+2;
+    #added hack to avoid overflow that almost never happen!!
+    while aa > 160: # above 160 math.gamma(aa) is just numerical error and above 180 overflow 
+        var=var+0.001
+        aa=np.true_divide(np.power(mu, 2),var)+2;
+    
     return aa
 
-def betaIG(mu,var):
-    aa=np.multiply(mu,np.true_divide(np.power(mu, 2),var)+1);
+def betaIG(mu,var): 
+    bb=np.multiply(mu,np.true_divide(np.power(mu, 2),var)+1);
+    
+    #loop to make sure we make the same alpha as in previous function in case it needed to be hacked to avoid overflow
+    aa=np.true_divide(np.power(mu, 2),var)+2;
+    #added hack to avoid overflow that almost never happen!!
+    while aa > 160: # above 160 math.gamma(aa) is just numerical error and above 180 overflow 
+        var=var+0.001
+        aa=np.true_divide(np.power(mu, 2),var)+2;
+        bb=np.multiply(mu,np.true_divide(np.power(mu, 2),var)+1);
+
+    return bb
+
+def alphaIG_raw(mu,var):
+    aa=np.true_divide(np.power(mu, 2),var)+2;    
     return aa
+
+def betaIG_raw(mu,var): 
+    bb=np.multiply(mu,np.true_divide(np.power(mu, 2),var)+1);
+    return bb
+
 
 def alphaGm(mu,var):
     aa=np.true_divide(np.power(mu, 2),var);